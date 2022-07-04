f8794b964986992b8737da72e0d5fe605b0c4b8e
Ranga9346 <108648710+Ranga9346@users.noreply.github.com>
2022-07-04 15:31:11 +0530

    Task-0407220330
    one java file added

---
+++ one.java
@@ -0,0 +1,12 @@
+import java.lang();
+
+class one
+{
+  public static void main(string[]args)
+  {
+    system.out.println("welocome to java Application-1");
+    system.out.println("welocome to java Application-1");
+    system.out.println("welocome to java Application-1");
+    system.out.println("welocome to java Application-1");
+  }
+}
